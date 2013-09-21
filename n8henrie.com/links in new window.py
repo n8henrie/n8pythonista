368c0c4408100f0b86a3920466dedf053321dc1a
Nathan Henrie <n8henrie@gmail.com>
2013-09-21 07:08:37 -0600

    Getting rid of old stuff.

--- n8henrie.com/links in new window.py
+++
@@ -1,5 +0,0 @@
-import clipboard
-import re
-
-myString = clipboard.get()
-clipboard.set(re.sub(r'(?<!<a target="_blank") href="(?!http://(www\.)?n8henrie\.com)', ' target="_blank" href="', myString))
